e48652e61e17ade72377bb08bad87adc69ecf0d9
andrex903 <50050676+andrex903@users.noreply.github.com>
2024-02-02 11:53:25 +0100

    Update CameraPrototype.cs

--- Runtime/Camera/CameraPrototype.cs
+++ Runtime/Camera/CameraPrototype.cs
@@ -4,7 +4,7 @@ namespace Redeev.PrototypingTools
 {
     public class CameraPrototype : MonoBehaviour
     {
-        [SerializeField] Transform target;
+        public Transform target;
 
         public Vector3 offset = new(-3f, 6f, -3f);
         public float fieldOfView = 55f;
@@ -26,7 +26,7 @@ namespace Redeev.PrototypingTools
 
         private void CenterCamera()
         {
-            transform.SetPositionAndRotation(target.position + offset, Quaternion.LookRotation(target.position - transform.position));
+            if (target) transform.SetPositionAndRotation(target.position + offset, Quaternion.LookRotation(target.position - transform.position));
         }
 
         private void Update()
